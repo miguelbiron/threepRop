3e5b4d95ea3dc7b55cd2d5649d5bdd271b15fd04
Miguel Biron <miguelbl9@gmail.com>
2019-01-17 09:11:07 -0800

    Added example dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example dataset
+#'
+#' A dataset containing an affinity matrix and a vector of labels. It is part of
+#' the code provided by the authors.
+#'
+#' @format A list with:
+#' \describe{
+#'   \item{net}{the affinity matrix}
+#'   \item{labels}{the labels}
+#' }
+#' @source \url{http://ai.stanford.edu/~saram/3propcode.zip}
+"example_dta"
